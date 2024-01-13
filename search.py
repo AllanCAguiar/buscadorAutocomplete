8a498778f134083cc8f935f8bb3fc692d83c9e1b
AllanCAguiar <allan.aguiar@icomp.ufam.edu.br>
2024-01-12 21:29:50 -0400

    Typo

---
+++ search.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify, render_template
+from elasticsearch import Elasticsearch
+
+app = Flask(__name__)
+es = Elasticsearch([{'host': 'localhost', 'port': 9200, 'scheme':'http'}])
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('q')
+
+    if not query:
+        return jsonify({'error': 'Query parameter "q" is required'}), 400
+
+    body = {
+            'query': {
+                'match': {
+                    'texto': {
+                        'query': query,
+                        'fuzziness': 1
+                    }
+                }
+            }
+        }
+
+    result = es.search(index='buscas', body=body)  
+    hits = result['hits']['hits']
+    results = [hit['_source']['texto'] for hit in hits]
+    return jsonify(results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ test.sh
@@ -0,0 +1,2 @@
+vegeta attack -duration=60s -rate=180 -targets=targets.txt -output=results/attack.bin
+vegeta report attack.bin
